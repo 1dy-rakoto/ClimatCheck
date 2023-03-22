309eb0162dba4526bc9d7da4cf68084f5aa31456
rakoto <endytolojanahary@gmail.com>
2023-03-22 23:05:02 +0300

    Finalisation de ClimateCheck 1.0.0

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,TextInput} from 'react-native';
+import Home from './Components/Home';
+import About from './Components/About';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
 
+
+
+const Drawer=createDrawerNavigator();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <NavigationContainer>
+      <Drawer.Navigator useLegacyImplementation screenOptions={{headerShown:false}}>
+        <Drawer.Screen name="Home" component={Home}/>
+        <Drawer.Screen name="About" component={About}/>
+      </Drawer.Navigator>
       <StatusBar style="auto" />
-    </View>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
